9296d84535e7337deb513b9b3b2a0856ec8202da
Muhammad Hashim Tariq <hashimtg111@gmail.com>
2025-11-08 15:53:53 +0500

    Heap.cpp completed with a minor error but perfect score

--- Part1/src/Heap.cpp
+++ Part1/src/Heap.cpp
@@ -6,26 +6,26 @@ using namespace std;
 
 MinHeap::MinHeap(int cap)
 {
-    this->harr = new int(cap);
+    this->harr = new int[cap];
     this->capacity = cap;
     this->heap_size = 0;
 } 
 
 MinHeap::~MinHeap()
 {
-    if(this->harr)
+    if(this->heap_size > 0)
     {
         delete[] this->harr;
-        this->capacity = 0;
-        this->heap_size = 0;
     }
+    this->capacity = 0;
+    this->heap_size = 0;
 }
 
 MinHeap::MinHeap(const MinHeap& other)
 {
     this->capacity = other.capacity;
     this->heap_size = other.heap_size;
-    this->harr = new int(this->capacity);
+    this->harr = new int[this->capacity];
     for(int i = 0; i < this->heap_size ; i++)
     {  
         this->harr[i] = other.harr[i];
@@ -36,7 +36,7 @@ MinHeap& MinHeap::operator=(const MinHeap& other)
 {
     this->capacity = other.capacity;
     this->heap_size = other.heap_size;
-    this->harr = new int(this->capacity);
+    this->harr = new int[this->capacity];
     for(int i = 0; i < this->heap_size ; i++)
     {  
         this->harr[i] = other.harr[i];
@@ -51,15 +51,16 @@ int MinHeap::extractMin()
         int min = this->harr[0];
         this->harr[0] = this->harr[this->heap_size - 1];
         this->heap_size--;
-        this->minHeapify(0);
+        this->minHeapify(0); // this sends the large swapped element down.
+        this->minHeapify(0); // this allows the actual minimum to come to the root.
         return min;
     }
-    return -1111;
+    return INT_MAX;
 }
 
 void MinHeap::decreaseKey(int i, int new_val)
 {
-    if(this->harr[i])
+    if(i < this->heap_size)
     {
         this->harr[i] = new_val;
         this->minHeapify(i);
@@ -68,7 +69,7 @@ void MinHeap::decreaseKey(int i, int new_val)
 
 void MinHeap::increaseKey(int i, int new_val)
 {
-    if(this->harr[i])
+    if(i >= 0 && i < this->heap_size)
     {
         this->harr[i] = new_val;
         this->minHeapify(i);
@@ -77,7 +78,7 @@ void MinHeap::increaseKey(int i, int new_val)
 
 int MinHeap::getMin() const
 {
-    if(this->harr[0])
+    if(this->heap_size > 0)
     {
         return this->harr[0];
     }
@@ -89,47 +90,76 @@ int MinHeap::getMin() const
 
 void MinHeap::deleteKey(int i)
 {
-    if(i < this->heap_size)
+    if(i >= 0 && i < this->heap_size)
     {
-        this->harr[i] = INT_MIN;
-        this->minHeapify(0);
-        this->extractMin();
+        this->harr[i] = this->harr[this->heap_size - 1];
+        this->heap_size--;
+        this->minHeapify(i);
     }
 }
 
 void MinHeap::insertKey(int k)
 {
-    if(this->heap_size == this->capacity)
+    if(this->heap_size >= this->capacity)
     {
-        int* new_harr = new int(this->capacity*2);
+        int* new_harr = new int[this->capacity*2];
         for(int i = 0; i < this->size(); i++)
         {
             new_harr[i] = this->harr[i];
         }
-        delete this->harr;
+        delete[] this->harr;
         this->harr = new_harr;
         this->capacity *= 2;
     }
     this->harr[this->heap_size] = k;
     this->heap_size++;
+    this->minHeapify(this->heap_size - 1);
 }
 
+// void MinHeap::minHeapify(int i)
+// {
+//     if(i < 0 || i >= this->size())
+//     {
+//         return;
+//     }
+//     while(i >= 0 && this->harr[this->parent(i)] > this->harr[i])
+//     {
+//         this->swap(&this->harr[this->parent(i)], &this->harr[i]);
+//         i = this->parent(i);
+//     }
+//     while((this->left(i) < this->size() && this->harr[this->left(i)] < this->harr[i]) || (this->right(i) < this->size() && this->harr[this->right(i)] < this->harr[i]))
+//     {
+//         if(this->harr[this->left(i)] < this->harr[i])
+//         {
+//             this->swap(&this->harr[this->left(i)], &this->harr[i]);
+//             i = this->left(i);
+//         }
+//         else
+//         {
+//             this->swap(&this->harr[this->right(i)], &this->harr[i]);
+//             i = this->right(i);
+//         }
+//     }
+// }
 void MinHeap::minHeapify(int i)
 {
-    if(this->parent(i) >=0 && this->harr[this->parent(i)] > this->harr[i])
+    if(i < 0 || i >= this->size())
     {
-        //downward Trickling case on parent(i)
+        return;
     }
-    else if(this->left(i) < this->size() && this->harr[this->left(i)] < this->harr[i])
+    if(this->parent(i) >= 0 && this->harr[this->parent(i)] > this->harr[i])
     {
-        //upward Bubbling case on right(i)
+        this->swap(&this->harr[this->parent(i)], &this->harr[i]);
+        this->minHeapify(this->parent(i));
     }
-    else if(this->right(i) < this->size() && this->harr[this->right(i)] < this->harr[i])
+    if(this->left(i) < this->size() && (this->harr[this->left(i)] < this->harr[i]))
     {
-        //upward Bubbling case on left(i)
+        this->swap(&this->harr[this->left(i)], &this->harr[i]);
+        this->minHeapify(this->left(i));
     }
-    else
+    if(this->right(i) < this->size() && (this->harr[this->right(i)] < this->harr[i]))
     {
-        //all good
+        this->swap(&this->harr[this->right(i)], &this->harr[i]);
+        this->minHeapify(this->right(i));
     }
 }
