5372e2a34be61e2954b09b75713fe878280fb493
Muhammad Hashim Tariq <hashimtg111@gmail.com>
2025-11-13 17:48:15 +0500

    Linear Probing implementation complete (12/40)

--- Part1/src/HashTables.cpp
+++ Part1/src/HashTables.cpp
@@ -1,6 +1,5 @@
 #include "HashTables.h"
 
-
 // =======================
 // Constructor
 // =======================
@@ -8,6 +7,18 @@ template <typename T>
 HashTable<T>::HashTable(CollisionHandle strategy)
 {
     collision_strategy = strategy;
+    this->table_size = 20;
+    this->num_elements = 0;
+    this->loadFactor = 0.0;
+    this->loadFactorThreshold = 0.85;
+    if (strategy == LINEAR_PROBING || strategy == QUADRATIC_PROBING)
+    {
+        this->probing_table = new KeyValuePair[this->table_size];
+    }
+    else
+    {
+        this->chaining_table.resize(this->table_size);
+    }
 }
 
 // =======================
@@ -16,7 +27,13 @@ HashTable<T>::HashTable(CollisionHandle strategy)
 template <typename T>
 HashTable<T>::~HashTable()
 {
-
+    if (this->collision_strategy == LINEAR_PROBING || this->collision_strategy == QUADRATIC_PROBING)
+    {
+        if (this->table_size > 0)
+        {
+            delete[] this->probing_table;
+        }
+    }
 }
 
 // =======================
@@ -25,9 +42,8 @@ HashTable<T>::~HashTable()
 template <typename T>
 void HashTable<T>::calculateLoadFactor()
 {
-    
+    this->loadFactor = this->num_elements / this->table_size;
 }
-
 
 // =======================
 // Linear Probing Methods
@@ -35,19 +51,52 @@ void HashTable<T>::calculateLoadFactor()
 template <typename T>
 void HashTable<T>::insertLinearProbing(int key, T value)
 {
-
+    int idx = this->hashFunction1(key);
+    while (!this->probing_table[idx].isEmpty)
+    {
+        idx++;
+        idx %= this->table_size;
+    }
+    this->probing_table[idx] = KeyValuePair(key, value, false);
+    this->num_elements++;
+    this->calculateLoadFactor();
+    if (this->loadFactor > this->loadFactorThreshold)
+    {
+        this->resizeAndRehash();
+    }
 }
-
 template <typename T>
 T HashTable<T>::searchLinearProbing(int key)
 {
-    return T();
+    int idx = this->hashFunction1(key);
+    while (!this->probing_table[idx].isEmpty)
+    {
+        if (this->probing_table[idx].key == key)
+        {
+            return this->probing_table[idx].value;
+        }
+        idx++;
+    }
+    return this->probing_table[idx].value;
 }
 
 template <typename T>
 void HashTable<T>::removeLinearProbing(int key)
-{
-    
+{   
+    int idx = this->hashFunction1(key);
+    for(int i = 0; i < this->table_size; i++)
+    {
+        if (this->probing_table[idx].key == key)
+        {
+            KeyValuePair temp;
+            this->probing_table[idx]  = temp;
+            this->num_elements--;
+            this->calculateLoadFactor();
+            return;
+        }
+        idx++;
+        idx = idx%this->table_size;
+    }
 }
 
 // =======================
@@ -56,7 +105,6 @@ void HashTable<T>::removeLinearProbing(int key)
 template <typename T>
 void HashTable<T>::insertQuadraticProbing(int key, T value)
 {
-    
 }
 
 template <typename T>
@@ -68,7 +116,6 @@ T HashTable<T>::searchQuadraticProbing(int key)
 template <typename T>
 void HashTable<T>::removeQuadraticProbing(int key)
 {
-    
 }
 
 // =======================
@@ -77,7 +124,6 @@ void HashTable<T>::removeQuadraticProbing(int key)
 template <typename T>
 void HashTable<T>::insertSeparateChaining(int key, T value)
 {
-    
 }
 
 template <typename T>
@@ -89,7 +135,37 @@ T HashTable<T>::searchSeparateChaining(int key)
 template <typename T>
 void HashTable<T>::removeSeparateChaining(int key)
 {
+}
 
+template <typename T>
+void HashTable<T>::resizeAndRehash()
+{  
+    if (this->collision_strategy == LINEAR_PROBING)
+    {
+        KeyValuePair* old_table = this->probing_table;
+        int old_size = this->table_size;
+        this->probing_table = new KeyValuePair[this->table_size * 2];
+        this->num_elements = 0;
+        this->table_size *= 2;
+        for (int i = 0; i < old_size; i++)
+        {
+            if (!old_table[i].isEmpty)
+            {
+                this->insert(old_table[i].key, old_table[i].value);
+            }
+        }
+        delete[] old_table;
+
+        this->calculateLoadFactor();
+    }
+    else if(this->collision_strategy == QUADRATIC_PROBING)
+    {
+
+    }
+    else if(this->collision_strategy == SEPARATE_CHAINING)
+    {
+
+    }
 }
 
 // =======================
@@ -100,15 +176,15 @@ void HashTable<T>::insert(int key, T value)
 {
     switch (collision_strategy)
     {
-        case LINEAR_PROBING:
-            insertLinearProbing(key, value);
-            break;
-        case QUADRATIC_PROBING:
-            insertQuadraticProbing(key, value);
-            break;
-        case SEPARATE_CHAINING:
-            insertSeparateChaining(key, value);
-            break;
+    case LINEAR_PROBING:
+        insertLinearProbing(key, value);
+        break;
+    case QUADRATIC_PROBING:
+        insertQuadraticProbing(key, value);
+        break;
+    case SEPARATE_CHAINING:
+        insertSeparateChaining(key, value);
+        break;
     }
     calculateLoadFactor();
 }
@@ -121,12 +197,12 @@ T HashTable<T>::search(int key)
 {
     switch (collision_strategy)
     {
-        case LINEAR_PROBING:
-            return searchLinearProbing(key);
-        case QUADRATIC_PROBING:
-            return searchQuadraticProbing(key);
-        case SEPARATE_CHAINING:
-            return searchSeparateChaining(key);
+    case LINEAR_PROBING:
+        return searchLinearProbing(key);
+    case QUADRATIC_PROBING:
+        return searchQuadraticProbing(key);
+    case SEPARATE_CHAINING:
+        return searchSeparateChaining(key);
     }
     return T(); // Default return
 }
@@ -139,15 +215,15 @@ void HashTable<T>::remove(int key)
 {
     switch (collision_strategy)
     {
-        case LINEAR_PROBING:
-            removeLinearProbing(key);
-            break;
-        case QUADRATIC_PROBING:
-            removeQuadraticProbing(key);
-            break;
-        case SEPARATE_CHAINING:
-            removeSeparateChaining(key);
-            break;
+    case LINEAR_PROBING:
+        removeLinearProbing(key);
+        break;
+    case QUADRATIC_PROBING:
+        removeQuadraticProbing(key);
+        break;
+    case SEPARATE_CHAINING:
+        removeSeparateChaining(key);
+        break;
     }
     calculateLoadFactor();
 }
@@ -165,3 +241,6 @@ void HashTable<T>::displayProbingTable()
             cout << "[" << i << "] -> EMPTY\n";
     }
 }
+
+template class HashTable<int>;
+template class HashTable<string>;
